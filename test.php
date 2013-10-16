68a7dfdafd615231fa4f74ddba98e964577fefd6
wondertalik <wondertalik@gmail.com>
2013-10-16 14:25:29 -0700

    second commit

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+ /**
+ * Этот файл содержит test.php class.
+ *
+ * @author WonderTalik <wondertalik@gmail.com>
+ * @link http://it.bitmaster.dp.ua/
+ * Date: 16.10.13
+ * @copyright 2013
+ */
+
+echo 1;
